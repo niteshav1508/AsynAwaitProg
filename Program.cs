f596a6c445eb3d8b29f6c1fb51288494246bd630
Nitesh Vihirkar <nitesh.vihirkar@qentelli.com>
2023-09-28 11:22:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
